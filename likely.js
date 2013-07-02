557ceecd4b0c9ebf092360ebc2b240d485efe630
sean <sean.byrnes@gmail.com>
2013-07-01 21:16:00 -0700

    Integrated Sylvester and implemented some tests of utility functions.

--- likely.js
+++ likely.js
@@ -3,47 +3,73 @@
  * 
  * Javascript library for collaborative filtering / recommendation engine. 
  */
+var sylvester = require('sylvester');
  
-DESCENT_STEPS = 2000; // number of iterations to execute gradient descent 
-ALPHA = 0.0005;       // learning rate, should be small
-BETA = 0.0007;        // regularization factor, should be small
- 
-module.exports.train(inputMatrix)
+// Learning parameters
+var DESCENT_STEPS = 2000; // number of iterations to execute gradient descent 
+var ALPHA = 0.0005;       // learning rate, should be small
+var BETA = 0.0007;        // regularization factor, should be small
+var k = 2; 				  // number of features to simulate
+
+// Model storage
+var P_model;
+var Q_model;
+var estimatedResult;
+
+// Trains the model on the given input by composing two matrices P and Q which
+// approximate the input through their product. 
+module.exports.train = function(inputMatrix)
 {
   // Generate random P and Q based on the dimensions of inputMatrix
   N = inputMatrix.length;    // number of rows 
   M = inputMatrix[0].length; // number of columns
-  k = 2;                     // number of features to simulate
   
-  P = generateRandomArray(N, k);
-  Q = generateRandomArray(M, k);
+  P_model = generateRandomMatrix(N, k);
+  Q_model = generateRandomMatrix(M, k);
   
   for(i = 0; i < DESCENT_STEPS; i++)
   {
     // determine error
-    var error = calculateError(P, Q, inputMatrix);
+    var error = calculateError(P_model, Q_model, inputMatrix);
     
     // update P and Q accordingly
     P_prime = update(P, error);
     Q_prime = update(Q, error);
     
-    P = P_prime;
-    Q = Q_prime;
+    P_model = P_prime;
+    Q_model = Q_prime;
   }
   
   // produce the final estimation by multiplying P and Q
-  var estimatedArray = P x Q; 
+  estimatedResult = P_model.x(Q_model); 
 }
 
 // Generates a random Matrix of size rows x columns
-module.exports.generateRandomArray(rows, columns)
+// TODO: Adjust the range of generated random values
+module.exports.generateRandomMatrix = function(rows, columns)
+{
+	return sylvester.Matrix.Random(rows, columns);
+}
+
+// Computes the error from model matrices P and Q against the given input. 
+// Result is a matrix of size input.rows by input.columns
+module.exports.calculateError = function(P, Q, input)
+{
+	// calculate R' as the result of P x Q
+	var R_prime = P.x(Q); 
+	
+	// Error is R - R'
+	return input.subtract(R_prime);
+}
+
+function update(Matrix, error)
 {
 
 }
 
 // For a given entity index, returns all item ids that had no rating for that item. 
 //     Results are sorted in order of descenting estimated value.
-getRecommendations(index)
+module.exports.getRecommendations = function(index)
 {
   
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,36 @@
-exports['test String#length'] = function(beforeExit, assert){
-    assert.equal(6, 'foobar'.length);
+/**
+ * Tests for the Likely.js recommendation library.
+ */
+var sylvester = require('sylvester');
+var Recommender = require('../likely.js');
+
+exports['test generateRandomMatrix#size'] = function(beforeExit, assert){
+	var M1 = Recommender.generateRandomMatrix(4,3);
+
+    assert.equal(4, M1.rows());
+    assert.equal(3, M1.cols());
+    
+	var M2 = Recommender.generateRandomMatrix(1,1);
+
+    assert.equal(1, M2.rows());
+    assert.equal(1, M2.cols());
+};
+
+exports['test generateRandomMatrix#random'] = function(beforeExit, assert){
+	var M1 = Recommender.generateRandomMatrix(4,3);
+	var M2 = Recommender.generateRandomMatrix(4,3);
+	
+	assert.equal(false, M1.eql(M2));
+};
+
+exports['test calculateError#values'] = function(beforeExit, assert){
+	var P = $M([[2, 2],[2, 2],[2, 2]]);
+	var Q = $M([[2, 2, 2],[2, 2, 2]]);
+	var input = $M([[8, 8, 8],[8, 8, 8],[8, 8, 8]]);
+	
+	var error = Recommender.calculateError(P, Q, input);
+	
+	var expectedError = sylvester.Matrix.Zeros(3, 3);
+	
+	assert.equal(true, expectedError.eql(error));
 };
